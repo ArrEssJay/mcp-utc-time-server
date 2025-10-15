c887f5b2848ab4c89d656ae7a45fd97cfa87bf48
Rowan Jones <rowan@impermanent.io>
2025-10-16 08:56:46 +1100

    remove ansi colour in logging

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,19 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Initialize logging to stderr
+    // Initialize logging to stderr without ANSI colors
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "mcp_utc_time_server=debug,info".into()),
+                .unwrap_or_else(|_| "mcp_utc_time_server=info,rmcp=warn".into()),
         )
-        .with(tracing_subscriber::fmt::layer().with_writer(std::io::stderr))
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_writer(std::io::stderr)
+                .with_ansi(false) // Disable ANSI color codes
+                .with_target(false) // Disable target module names
+                .compact(), // Use compact format
+        )
         .init();
 
     // Run the server with official SDK
